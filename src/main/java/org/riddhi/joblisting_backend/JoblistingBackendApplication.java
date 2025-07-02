996b97c99b937188eba0770424c40251641f0569
Riddhi Patil <riddhipatil1905@gmail.com>
2025-07-03 01:10:52 +0530

    initial backend commit

---
+++ src/main/java/org/riddhi/joblisting_backend/JoblistingBackendApplication.java
@@ -0,0 +1,13 @@
+package org.riddhi.joblisting_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoblistingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoblistingBackendApplication.class, args);
+    }
+
+}
