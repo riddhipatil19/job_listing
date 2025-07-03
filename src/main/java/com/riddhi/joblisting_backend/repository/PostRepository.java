88086d842cad429ac57c079cefbda78cd376a69d
Riddhi Patil <riddhipatil1905@gmail.com>
2025-07-04 00:18:05 +0530

    adding frontend

--- src/main/java/com/riddhi/joblisting_backend/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.riddhi.joblisting_backend.repository;
-
-import com.riddhi.joblisting_backend.model.Post;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PostRepository extends MongoRepository<Post, String> {
-}
